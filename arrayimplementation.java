4ad4c54be2c4def12d826ef9e2b4471f610cb75e
Mushrafali22 <amushraf788@gmail.com>
2026-03-17 16:13:16 +0530

    File \modified

--- arrayimplementation.java
+++ arrayimplementation.java
@@ -21,18 +21,65 @@ class dynamicArray{
     }
     private void enlargeArray(){
         capacity*=2;
-        java.util.Arrays.copyOf(arr, capacity);
+        arr=java.util.Arrays.copyOf(arr, capacity);
     }
     public void display(){
+        if(size==0){
+            System.out.println("Array is empty");
+        }
         for(int i=0;i<size;i++){
             System.out.print(arr[i]+" ");
         }
+    }
+
+    public void insertAtPos(int pos,int val){
+        if(size==capacity){
+            enlargeArray();
+        }
+        for(int i=size-1;i>=pos;i--){
+            arr[i+1]=arr[i];
+        }
+        arr[pos]=val;
+        size++;
+    }
+
+    public void deleteAtPos(int pos){
+        for(int i=pos+1;i<size-1;i++){
+            arr[i-1]=arr[i];
+        }
+        size--;
+
+        if(capacity>intialcapacity && capacity > 3*size){
+            ShrinkArray1();
+        }
+    }
+
+    private void ShrinkArray1() {
+       capacity/=2;
+       arr=java.util.Arrays.copyOf(arr,capacity);
+    }
+
+    public void deleteFinal(){
+        if(size==0){
+            System.out.println("Array is empty");
+        }
+        size--;
+        if(capacity>intialcapacity && capacity > 3*size){
+            ShrinkArray1();
+        }
+    }
+
+    public void deleteFirst(){
+        for(int i=0;i<size-1;i++){
+            arr[i]=arr[i+1];
+        }
+        size--;
     }
 }
 
 public class arrayimplementation {
     public static void main(String[] args) {
-        int val,pos;
+        int val,pos,index;
         dynamicArray list = new dynamicArray();
         Scanner sc=new Scanner(System.in);
         while (true) {
@@ -41,7 +88,13 @@ public class arrayimplementation {
         System.out.println("2. Display Data");
         System.out.println("3. Insert at Specfic location");
         System.out.println("4. Delete at Specfic Location");
-        System.out.println("5. Exit");
+        System.out.println("5. Delete at end");
+        System.out.println("6. Delete from beginning");
+        System.out.println("7. insert at beginning");
+        System.out.println("8. get the index");
+        System.out.println("9. update the index");
+        System.out.println("10. Search the index");
+        System.out.println("11. Exit");
         System.out.println("\n---------------------------------------------\n");
         System.out.println("Enter the choice : \n");
         int chioce = sc.nextInt();
@@ -60,17 +113,46 @@ public class arrayimplementation {
                    }
                    System.out.println("Enter the Data : ");
                    val=sc.nextInt();
-                  // list.insertAtPos(pos,val);
+                   list.insertAtPos(pos,val);
                    break;
-            case 4:System.out.println("Enter the Postion to be insert : ");
+            case 4:System.out.println("Enter the Postion to be delete : ");
                    pos=sc.nextInt();
                    if(pos<0){
                     System.out.println("Invalid Postion");
                     break;
                    }
-                 //1  list.deleteAtPos(pos);
+                   list.deleteAtPos(pos);
                    break;
-            case 5:System.exit(0);    
+            case 5:System.out.println("Delete the final element");
+                   list.deleteFinal();
+                   break;
+            case 6:System.out.println("Delete from Beginning");
+                   list.deleteFirst();
+                   break;
+            case 7:System.out.println("Insert at Beginning");
+                   System.out.println("Enter the Value : ");
+                   val=sc.nextInt();
+                   //list.insertFirst();
+                   break;
+            case 8:System.out.println("Get the Index");
+                   //list.getIndex();
+                   break;
+            case 9:System.out.println("Update at index");
+                   index=sc.nextInt();
+                   if(index<0){
+                    System.out.println("Invalid Index");
+                    break;
+                   }
+                   System.out.println("Enter the Data : ");
+                   val=sc.nextInt();
+                   //list.updateIndex(pos,val);
+                   break;
+            case 10:System.out.println("Search the Index");
+                    System.out.println("Enter the Data : ");
+                    val=sc.nextInt();
+                    //list.search(val);
+                    break;
+            case 11:System.exit(0);    
             default:System.out.println("Invalid Choice");
                 break;
             }
